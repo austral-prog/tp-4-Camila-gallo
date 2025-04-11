9068461e159c39b38aa80d4d6c58995c6d39affa
Camila-gallo <cgallopiva1@mail.austral.edu.ar>
2025-04-11 12:45:22 -0300

    Update line.py

--- line.py
+++ line.py
@@ -11,8 +11,8 @@ def line():
     print("")
     Y1= a*x1+b
     Y2= a*x2+b
-    print('Para la siguiente ecuación: \tY = 2.3X + -4.0')
-    print(f"Dados los siguientes puntos: \nP1 ({x1},{Y1}) \nP2 ({x2},{Y2})")
+    print('Para la siguiente ecuación: \n\tY = 2.3X + -4.0')
+    print(f"Dados los siguientes puntos: \n\tP1 ({x1},{Y1}) \n\tP2 ({x2},{Y2})")
     distancia = math.sqrt((x2 - x1)**2 + (Y2 - Y1)**2)
     print("")
     print(f"La distancia entre ellos es {distancia}")
